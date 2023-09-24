1f81ded3e06db64481a46e41d588f21d28ab603f
AvihayR <avihayrabi@gmail.com>
2023-09-24 12:19:39 +0300

    Add GoogleMap with Markers and Btns

--- src/pages/GoogleMap.jsx
+++ src/pages/GoogleMap.jsx
@@ -1,30 +1,60 @@
+import googleMapReact from 'google-map-react';
 import GoogleMapReact from 'google-map-react';
+import { useState } from 'react';
+const apiKey = import.meta.env.VITE_GOOGLE_API_KEY
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const Marker = ({ text }) => <div className='logo'>{text}</div>;
+
+
 
 export function GoogleMap() {
+    const [currLocation, setLocation] = useState({ center: null, zoom: null })
+
+    console.log(currLocation)
+
     const defaultProps = {
         center: {
-            lat: 10.99835602,
-            lng: 77.01502627
+            lat: 32.08824718561407,
+            lng: 34.773570683968735
         },
         zoom: 11
     };
 
+    function goToStore(lat, lng) {
+        setLocation({ center: { lat, lng }, zoom: 15 })
+    }
+
     return (
         // Important! Always set the container height explicitly
-        <div style={{ height: '100vh', width: '100%' }}>
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: "" }}
-                defaultCenter={defaultProps.center}
-                defaultZoom={defaultProps.zoom}
-            >
-                <AnyReactComponent
-                    lat={59.955413}
-                    lng={30.337844}
-                    text="My Marker"
-                />
-            </GoogleMapReact>
-        </div>
+        <>
+            <button onClick={() => { goToStore(32.08824718561407, 34.773570683968735) }}>
+                TLV - Israel
+            </button>
+            <button onClick={() => { goToStore(35.18981167874425, 25.718217726175347) }}>
+                Crete - Greece
+            </button>
+            <div style={{ height: '100vh', width: '100%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: apiKey }}
+                    defaultCenter={defaultProps.center}
+                    defaultZoom={defaultProps.zoom}
+                    center={currLocation.center}
+                    zoom={currLocation.zoom}
+                >
+                    <Marker
+                        lat={32.08824718561407}
+                        lng={34.773570683968735}
+                        text="Toyz-N-Us🧸"
+                    />
+
+                    <Marker
+                        lat={35.18981167874425}
+                        lng={25.718217726175347}
+                        text="Toyz-N-Us🧸"
+                    />
+                </GoogleMapReact>
+            </div>
+        </>
+
     );
 }
