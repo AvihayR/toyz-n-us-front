9574a48b5d27233b6c6c69959f45e6b9e477d695
AvihayR <avihayrabi@gmail.com>
2023-09-30 02:27:28 +0300

    Add conditional rendering of userName to header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
-export const AppHeader = () => {
+export function AppHeader() {
+    const user = useSelector((state) => state.userModule.user)
+
     return <header className="app-header" >
         <>
             <h1 className='logo'>Toyz-<span>N</span>-Us</h1>
@@ -9,7 +12,7 @@ export const AppHeader = () => {
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/toy">Toys</NavLink>
                 <NavLink to="/about">About</NavLink>
-                <NavLink to="/login">Login</NavLink>
+                <NavLink to="/login">{!user ? 'Login' : user.fullname}</NavLink>
             </div>
         </>
     </header>
