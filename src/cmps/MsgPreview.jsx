1ccb4a61ae36e10045a7207090ad8fa04e581968
AvihayR <avihayrabi@gmail.com>
2023-09-30 17:42:15 +0300

    Add MsgPreview styling

--- src/cmps/MsgPreview.jsx
+++ src/cmps/MsgPreview.jsx
@@ -2,7 +2,7 @@ export function MsgPreview({ msg }) {
     const { txt, by } = msg
 
     return (
-        <article className="comment">
+        <article className="msg-preview">
             <h4 className="txt">{txt}</h4>
             <h5 className="by">By: {by.fullname}</h5>
         </article>
